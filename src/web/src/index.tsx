d131a31ce6937e56c6aa0e1bc8e3775378f031e1
Lucas <lucasschejtman@gmail.com>
2019-06-19 20:51:20 +1200

    removed devlounge logo

--- src/web/src/index.tsx
+++ src/web/src/index.tsx
@@ -1,29 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import * as serviceWorker from "./serviceWorker";
 
-import './App.css';
-import './index.css';
+import "./App.css";
+import "./index.css";
 
-import App from './App';
-import MenuAppBar from './MenuAppBar';
-import StockDetail from './StockDetail';
+import App from "./App";
+import MenuAppBar from "./MenuAppBar";
+import StockDetail from "./StockDetail";
 
-require('dotenv').config();
+require("dotenv").config();
 
 ReactDOM.render(
     <Router>
         <div>
             <MenuAppBar />
-            <header className="App-header"></header>
             <div>
                 <Route exact path="/" component={App} />
                 <Route exact path="/stock/:id" component={StockDetail} />
             </div>
         </div>
     </Router>,
-    document.getElementById('root')
+    document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
