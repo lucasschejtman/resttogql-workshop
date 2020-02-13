31c651bba99f7b4ad0c5c90ae93359015d6fdfcd
Lucas <lucasschejtman@gmail.com>
2020-02-13 15:28:48 +1300

    bump lambda to 10.x

--- src/infrastructure/lib/helper/functions.ts
+++ src/infrastructure/lib/helper/functions.ts
@@ -1,21 +1,21 @@
-import { execSync } from "child_process";
-import path = require("path");
-import cdk = require("@aws-cdk/core");
-import lambda = require("@aws-cdk/aws-lambda");
+import { execSync } from 'child_process';
+import path = require('path');
+import cdk = require('@aws-cdk/core');
+import lambda = require('@aws-cdk/aws-lambda');
 //import { AssetPackaging } from "@aws-cdk/assets";
-import { Duration } from "@aws-cdk/core";
+import { Duration } from '@aws-cdk/core';
 
 type FnProps = Partial<lambda.FunctionProps> & {
     // I'll remove this once there's a better integration with SAM
     hasDeps?: boolean;
 };
 
-const FN_PATH = "server";
+const FN_PATH = 'server';
 
-const installDeps = (dir: string) => execSync("npm install", { cwd: dir });
+const installDeps = (dir: string) => execSync('npm install', { cwd: dir });
 
 export const Function = (scope: cdk.Construct) => (id: string, props: FnProps) => {
-    const dir = path.join("..", FN_PATH, id);
+    const dir = path.join('..', FN_PATH, id);
 
     if (props.hasDeps) installDeps(dir);
 
@@ -24,8 +24,8 @@ export const Function = (scope: cdk.Construct) => (id: string, props: FnProps) =
         timeout: Duration.seconds(180),
         environment: props.environment || {},
         functionName: props.functionName || id,
-        handler: props.handler || "index.handler",
-        runtime: props.runtime || lambda.Runtime.NODEJS_8_10,
+        handler: props.handler || 'index.handler',
+        runtime: props.runtime || lambda.Runtime.NODEJS_10_X,
         code: props.code || new lambda.AssetCode(dir)
     });
 
